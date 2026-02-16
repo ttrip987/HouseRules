9b6e3c8fd128c2a871f920ec3b0b2954d10fb7ac
ttrip987 <ttrip987@gmail.com>
2026-02-16 16:29:16 -0500

    2/16/26 4:29pm

--- murdermysterygame/Assets/Scripts/Movement/Player Controller.cs
+++ murdermysterygame/Assets/Scripts/Movement/Player Controller.cs
@@ -3,17 +3,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 4f;
-    public bool canMove = true; 
+    public bool canMove = true;
 
     private Rigidbody2D rb;
     private Vector2 input;
-    private Vector2 lastMoveDir = Vector2.down;
+    private Animator animator;
 
-    private Animator anim;
+    private SpriteRenderer spriteRenderer;
 
     void Awake()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -21,34 +23,63 @@ public class PlayerController : MonoBehaviour
         if (!canMove)
         {
             input = Vector2.zero;
+            ResetDirectionBools();
+            animator.SetBool("IsMoving", false);
             return;
         }
 
-       
         input.x = Input.GetAxisRaw("Horizontal");
         input.y = Input.GetAxisRaw("Vertical");
 
-        
+        // Prevent diagonal movement
         if (input.x != 0)
-            input.y = 0;
+            input.y = 0; 
 
-        if (input != Vector2.zero)
-            lastMoveDir = input.normalized;
+        if (input.x > 0)
+            spriteRenderer.flipX = false;
+
+        else if (input.x < 0)
+            spriteRenderer.flipX = true;
+
+        UpdateAnimationBools();
     }
 
     void FixedUpdate()
     {
-        if (!canMove)
-        {
-            rb.velocity = Vector2.zero;
-            return;
-        }
-
-        rb.velocity = input.normalized * moveSpeed;
+        rb.velocity = canMove ? input.normalized * moveSpeed : Vector2.zero;
     }
 
-    public Vector2 GetFacingDirection()
+    void UpdateAnimationBools()
     {
-        return lastMoveDir;
+        ResetDirectionBools();
+
+        if (input == Vector2.zero)
+        {   
+            animator.SetBool("IsMoving", false);
+            return;
+        }
+        else{
+        animator.SetBool("IsMoving", true);
+        }
+
+    
+        if (input.y > 0.3)
+            animator.SetBool("MovingUp", true);
+        else if (input.y < - 0.3)
+            animator.SetBool("MovingDown", true);
+        else if (input.x > 0.3)
+            animator.SetBool("MovingRight", true);
+        else if (input.x < -0.3)
+            animator.SetBool("MovingLeft", true);
+    }
+
+  
+
+    void ResetDirectionBools()
+    {
+        animator.SetBool("MovingUp", false);
+        animator.SetBool("MovingDown", false);
+        animator.SetBool("MovingLeft", false);
+        animator.SetBool("MovingRight", false);
     }
 }
